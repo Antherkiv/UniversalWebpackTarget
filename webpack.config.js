fab45532680f8d1237face851df29622b3ec57b0
German M. Bravo <german.mb@gmail.com>
2018-05-16 10:45:09 -0500

    Config factory + nested libs

--- webpack.config.js
+++ webpack.config.js
@@ -3,102 +3,66 @@ const path = require('path');
 const webpack = require('webpack');
 const universalTarget = require('./universal-webpack');
 
+function factory(name, entry, target) {
+  return {
+    name: name,
+    devtool: 'source-map',
+    entry: {
+      [name]: entry
+    },
+    output: {
+      library: name,
+      filename: '[name].js',
+      publicPath: 'libs/' + name + '/',
+      path: path.resolve(__dirname, 'libs', name),
+    },
+    target: target,
+
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.json']
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: [
+            'cache-loader',
+            {
+              loader: 'ts-loader',
+            },
+          ],
+        },
+      ]
+    },
+
+    plugins: [
+      new webpack.NamedModulesPlugin(),
+      // new webpack.NamedChunksPlugin(),
+    ],
+
+    externals: {
+      'unicode/category/So': '{}', // Ignore unicode/category/So used (only in the server side) by node-slug.
+    },
+  }
+}
+
 module.exports = [
-  {
-    name: 'Base',
-    entry: {
-      'Base': [
-        'react',
-        'react-dom',
-      ]
-    },
-    output: {
-      library: 'Base',
-      filename: '[name].js',
-      publicPath: 'libs/',
-      path: path.resolve(__dirname, 'libs', 'Base'),
-    },
-    target: universalTarget({
-      dll: true,
-    }),
 
-    devtool: 'source-map',
+  factory('Base', [
+    'react',
+    'react-dom',
+  ], universalTarget({
+    dll: true,
+  })),
 
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.json']
-    },
-
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: [
-            'cache-loader',
-            {
-              loader: 'ts-loader',
-            },
-          ],
-        },
-      ]
-    },
-
-    plugins: [
-      new webpack.NamedModulesPlugin(),
-      // new webpack.NamedChunksPlugin(),
+  factory('main', [
+    './src/index.tsx',
+  ], universalTarget({
+    libsPath: path.resolve(__dirname, 'libs'),
+    imports: [
+      'Base'
     ],
+  })),
 
-    externals: {
-      'unicode/category/So': '{}', // Ignore unicode/category/So used (only in the server side) by node-slug.
-    },
-  },
-
-
-  {
-    name: 'main',
-    entry: {
-      'main': [
-        './src/index.tsx',
-      ],
-    },
-    output: {
-      library: 'main',
-      filename: '[name].js',
-      publicPath: 'libs/',
-      path: path.resolve(__dirname, 'libs')
-    },
-    target: universalTarget({
-      imports: [
-        'Base'
-      ],
-    }),
-
-    devtool: 'source-map',
-
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.json']
-    },
-
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: [
-            'cache-loader',
-            {
-              loader: 'ts-loader',
-            },
-          ],
-        },
-      ]
-    },
-
-    plugins: [
-      new webpack.NamedModulesPlugin(),
-      // new webpack.NamedChunksPlugin(),
-    ],
-
-    externals: {
-      'unicode/category/So': '{}', // Ignore unicode/category/So used (only in the server side) by node-slug.
-    },
-  },
 ]
