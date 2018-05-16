ae5bad6edc8bfa46506248a93237240d44ed4afb
German M. Bravo <german.mb@gmail.com>
2018-05-16 11:35:59 -0500

    Named chunk

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import * as ReactDOM from 'react-dom';
 
 import { Hello } from './components/Hello';
 
-import(/* webpackPrefetch: true */ './test').then(({ test }) => test());
+import(/* webpackChunkName: "test" */ './test').then(({ test }) => test());
 
 ReactDOM.render(<Hello compiler="TypeScript" framework="React" />, document.getElementById('example'));
