f27e09b879b67dbf8806d00c3d5a96b749a0cb7f
German M. Bravo <german.mb@gmail.com>
2018-05-18 18:00:46 -0500

    Example with preload/prefetch

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 
 import { Hello } from './components/Hello';
 
-import(/* webpackChunkName: "test1" */ './test1').then(({ test1 }) => {
+import(/* webpackChunkName: "test1", webpackPreload: true */ './test1').then(({ test1 }) => {
   test1('main');
-  import(/* webpackChunkName: "test2" */ './test2').then(({ test2 }) => {
+  import(/* webpackChunkName: "test2", webpackPrefetch: true */ './test2').then(({ test2 }) => {
     test2('main');
     import(/* webpackChunkName: "test3" */ './test3').then(({ test3 }) => {
       test3('main');
