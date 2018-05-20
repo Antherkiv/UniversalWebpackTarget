3e2129f8faf866f76c0d927b9ff6ed2f294f1258
German M. Bravo <german.mb@gmail.com>
2018-05-20 12:32:58 -0500

    eslint

--- LibManifestPlugin.js
+++ LibManifestPlugin.js
@@ -13,6 +13,7 @@
 	it uses all chunks only in the initial groups.
 */
 "use strict";
+/* eslint node/no-unpublished-require:0 node/no-extraneous-require:0 */
 
 const path = require("path");
 const asyncLib = require("neo-async");
