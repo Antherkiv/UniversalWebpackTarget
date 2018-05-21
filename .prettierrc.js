0fedd5bbdd0dc3d9f925c3748581263215fac04b
German M. Bravo <german.mb@gmail.com>
2018-05-21 17:01:34 -0500

    Normalized configs

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,5 @@
 module.exports = {
-    // singleQuote: true,
-    // trailingComma: "all",
-    // printWidth: 105
-	printWidth: 80,
-	useTabs: true,
-	tabWidth: 2
+    singleQuote: true,
+    trailingComma: "all",
+    printWidth: 105
 }
