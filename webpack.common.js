87fe124283600a187218116646d47429062403a9
German M. Bravo <german.mb@gmail.com>
2018-05-30 15:27:38 -0500

    Enable cache-loader

--- webpack.common.js
+++ webpack.common.js
@@ -36,7 +36,7 @@ function factory(options) {
         {
           test: /\.tsx?$/,
           use: [
-            // 'cache-loader',
+            'cache-loader',
             {
               loader: 'ts-loader',
             },
